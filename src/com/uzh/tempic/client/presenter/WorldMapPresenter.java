ae85041d889f2626a9080ee6bee3e0629b6150a6
Martin Bucher <manibucher@gmx.net>
2016-11-03 10:51:17 +0100

    Added WorldMapPresenter & View

---
+++ src/com/uzh/tempic/client/presenter/WorldMapPresenter.java
@@ -0,0 +1,91 @@
+package com.uzh.tempic.client.presenter;
+
+
+// TODO: Check if any libraries are not used
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.gwt.user.client.ui.Widget;
+import com.uzh.tempic.client.TempicServiceAsync;
+import com.uzh.tempic.client.presenter.Presenter;
+
+import java.util.List;
+
+/**
+ * Created by Martin on 2.11.16.
+ */
+
+public class WorldMapPresenter implements Presenter {
+
+    public interface Display {
+        HasClickHandlers getYearSlider();
+        //HasClickHandlers getDeleteButton();
+        //HasClickHandlers getList();
+        //void setData(List<String> data);
+        //int getClickedRow(ClickEvent event);
+        //List<Integer> getSelectedRows();
+        Widget asWidget();
+    }
+
+    private final TempicServiceAsync rpcService;
+    private final HandlerManager eventBus;
+    private final WorldMapPresenter.Display display;
+
+    public WorldMapPresenter(TempicServiceAsync rpcService, HandlerManager eventBus, WorldMapPresenter.Display view) {
+        this.rpcService = rpcService;
+        this.eventBus = eventBus;
+        this.display = view;
+    }
+
+
+    // Binds the interactions in the view to the presenter / eventbus
+
+    public void bind() {
+
+        display.getYearSlider().addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                // eventBus.fireEvent(new AddContactEvent());
+            }
+        });
+
+    }
+
+
+    // Renders the view
+
+    public void go(final HasWidgets container) {
+        bind();
+        container.clear();
+        container.add(display.asWidget());
+        fetchWorldMapData();
+    }
+
+
+    // Gets the data from the model
+
+    private void fetchWorldMapData() {
+       /* rpcService.getWorldDashboardData(new AsyncCallback<ArrayList<TemperatureData>>() {
+            public void onSuccess(ArrayList<TemperatureData> result) {
+                contactDetails = result;
+                sortContactDetails();
+                List<String> data = new ArrayList<String>();
+
+                for (int i = 0; i < result.size(); ++i) {
+                    data.add(contactDetails.get(i).getDisplayName());
+                }
+
+                display.setData(data);
+            }
+
+            public void onFailure(Throwable caught) {
+                Window.alert("Error fetching contact details");
+            }
+        });
+        */
+    }
+
+}
---
+++ src/com/uzh/tempic/client/view/WorldMapView.java
@@ -0,0 +1,55 @@
+package com.uzh.tempic.client.view;
+
+// TODO: Check if any libraries are not used
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.user.client.ui.*;
+import com.uzh.tempic.client.presenter.WorldMapPresenter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Martin on 2.11.16.
+ */
+public class WorldMapView extends Composite implements WorldMapPresenter.Display {
+
+    private final Button yearSlider = new Button("Slider for years");
+    private final FlexTable contentTable;
+
+    public WorldMapView(){
+
+        DecoratorPanel contentTableDecorator = new DecoratorPanel();
+        initWidget(contentTableDecorator);
+        contentTableDecorator.setWidth("100%");
+        contentTableDecorator.setWidth("18em");
+
+        contentTable = new FlexTable();
+        contentTable.setWidth("100%");
+        contentTable.getCellFormatter().addStyleName(0, 0, "worldDashboard-ListContainer");
+        contentTable.getCellFormatter().setWidth(0, 0, "100%");
+        contentTable.getFlexCellFormatter().setVerticalAlignment(0, 0, DockPanel.ALIGN_TOP);
+
+        // Create the menu
+        //
+        HorizontalPanel hPanel = new HorizontalPanel();
+        hPanel.setBorderWidth(0);
+        hPanel.setSpacing(0);
+        hPanel.setHorizontalAlignment(HorizontalPanel.ALIGN_LEFT);
+        contentTable.getCellFormatter().addStyleName(0, 0, "worldDashboard-ListMenu");
+        contentTable.setWidget(0, 0, hPanel);
+
+        contentTableDecorator.add(contentTable);
+
+    }
+
+    public HasClickHandlers getYearSlider() {
+        return yearSlider;
+    }
+
+    public Widget asWidget() {
+        return this;
+    }
+
+}
