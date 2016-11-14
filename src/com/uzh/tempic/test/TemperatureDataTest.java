b67a9483ba425643a8560451e8390cbc08be259b
Relaxia <kaufmann.robin@gmail.com>
2016-11-14 12:23:40 +0100

    Tried implementing first tastcase with JUnit4 for the TemperatureData Class

---
+++ src/com/uzh/tempic/test/TemperatureDataTest.java
@@ -0,0 +1,36 @@
+package com.uzh.tempic.test;
+
+import com.uzh.tempic.shared.TemperatureData;
+import junit.framework.TestCase;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Robin on 14.11.2016.
+ */
+public class TemperatureDataTest extends TestCase{
+
+
+    /* Set a Test for the Temperature Data Class */
+    public void testTemperatureData(){
+        Date date = new Date(2010, 1, 1);
+        Double avgTemperature = 10.0;
+        Double avgTemperatureUncertainty = 1.0;
+        String city = "TestCity";
+        String country = "TestCountry";
+        String latitude = "latitude";
+        String longitude = "longitude";
+        TemperatureData test = new TemperatureData(date, avgTemperature, avgTemperatureUncertainty, city, country, latitude, longitude);
+
+        assertEquals(date, test.getDate());
+        assertEquals(avgTemperature, test.getAvgTemperature());
+        assertEquals(avgTemperatureUncertainty, test.getAvgTemperatureUncertainty());
+        assertEquals(city, test.getCity());
+        assertEquals(country, test.getCountry());
+        assertEquals(latitude, test.getLatitude());
+        assertEquals(longitude, test.getLongitude());
+    }
+
+}
